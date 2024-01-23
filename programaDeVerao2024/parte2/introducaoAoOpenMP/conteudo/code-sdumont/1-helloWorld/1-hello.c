3a5def8afa78a0beff501b617c212c7fc7a760f4
Brenddon Andrade <brenddonandrade@gmail.com>
2024-01-23 11:59:20 -0300

    :pacakge: conteudo do curso de openMP

--- programaDeVerao2024/parte2/introducaoAoOpenMP/conteudo/code-sdumont/1-helloWorld/1-hello.c
+++ programaDeVerao2024/parte2/introducaoAoOpenMP/conteudo/code-sdumont/1-helloWorld/1-hello.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <omp.h>
 
 int main(){
 	int myid, nthreads;
 
-	myid = 0;
-	nthreads = 1;
+	#pragma omp parallel private(myid)
+	{
+		myid = omp_get_thread_num();
+		nthreads = omp_get_num_threads();
 
-	printf("%d of %d - hello world!\n", myid, nthreads);
-
+		printf("%d of %d - hello world!\n", myid, nthreads);
+	}
 	return 0;
 }
--- programaDeVerao2024/parte2/introducaoAoOpenMP/conteudo/code-sdumont/2-vectorSum/src/2-vsum.c
+++ programaDeVerao2024/parte2/introducaoAoOpenMP/conteudo/code-sdumont/2-vectorSum/src/2-vsum.c
@@ -1,9 +1,29 @@
 long long int sum(int *v, long long int N){
-	long long int i = 0, sum = 0;
+	long long int i = 0, sum = 0, sum_local = 0;
 	
-	#pragma omp parallel for reduction(+ : sum)
-	for(i = 0; i < N; i++)
+	//#pragma omp parallel for reduction(+ : sum)
+	//#pragma omp parallel for private(i)
+	//for(i = 0; i < N; i++)
+		// Race condition
+	//	#pragma omp atomic
+	//	sum += v[i];
+	
+	//#pragma omp parallel private(i, sum_local)
+	//{
+	//	sum_local = 0;
+	//	#pragma omp for
+	//	for(i=0 ; i < N ; i++)
+	//		sum_local += v[i];
+
+	//	#pragma omp atomic 
+	//	sum += sum_local;
+	//}
+			
+	#pragma omp parallel for private(i) reduction(+ : sum)
+	for (i=0 ; i < N; i++)
 		sum += v[i];
-	
+
+
+
 	return sum;
 }
